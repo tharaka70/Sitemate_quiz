bc78d2906d7218e632caacf5d8190e0ae0bb0ad4
Tharaka Dissanayake <tharakadissanayake70@gmail.com>
2023-10-05 19:21:11 +0530

    Fake json Db is added for json issues.

---
+++ SERVER/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, HTTPException, status
+
+# Fake DB for issues | Hardcoded for now since we are not using a DB
+issues_db = [
+    
+    {
+        "id": 1,
+        "title": "Add to cart feature",
+        "description": "Users should be able to add gift cards to their carts. Currently they can add only the items in clothing section."
+    },
+    {
+        "id": 2,
+        "title": "Currency issue fix",
+        "description": "currency values shown when currency changed are wrong. need to fix that."
+    },
+    {
+        "id": 3,
+        "title": "Validation Error in user form",
+        "description": "Users can enter text for the age text box. It should be limited only to enter numbers."
+    },
+    {
+        "id": 4,
+        "title": "Data type miss match issue",
+        "description": "Decription exceeds the limit given in the DB. Need to fix it."
+    },
+    {
+        "id": 5,
+        "title": "New feature: Pay with paypal",
+        "description": "Since many users has requested us to integrate paypal we need to add it."
+    }
+]
+
+app = FastAPI()
